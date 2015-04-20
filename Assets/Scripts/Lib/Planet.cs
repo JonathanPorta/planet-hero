330045b079e9b47fdd173e7c011584c1a159186d
Jonathan Porta <jonathan@jonathanPorta.com>
2015-04-19 20:48:45 -0700

    Hotfix for broken material and horribly pixelated splash image

--- Assets/Scripts/Lib/Planet.cs
+++ Assets/Scripts/Lib/Planet.cs
@@ -25,42 +25,43 @@ public class Planet : Gridable {
 		LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
 		if(lineRenderer == null){
 			lineRenderer = gameObject.AddComponent<LineRenderer>();
-			//float radius = orbit.distance;
-			//float thetaScale = 1.0f;             //Set lower to add more points
-			//int size = Mathf.RoundToInt(radius * Mathf.PI / thetaScale); //Total number of points in circle.
-
-			lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-			//lineRenderer.SetColors(c1, c2);
-			lineRenderer.SetWidth(0.2F, 0.2F);
-			//lineRenderer.SetVertexCount(360);
-
-			
-			//int i = 0;
-			//for(float theta = 0; theta < radius * Mathf.PI; theta += thetaScale) {
-			//	float x = radius * Mathf.Cos(theta);
-			//	float y = radius * Mathf.Sin(theta);
-			//	
-			//	Vector3 pos = new Vector3(x, y, 0);
-			//	lineRenderer.SetPosition(i, pos);
-			//	i+=1;
-			//}
-
-			float degreesPerSegment = 0.1f;
-			float currentAngle = 0.0f;
-			lineRenderer.SetVertexCount(Mathf.RoundToInt(360 / degreesPerSegment + 1));
-
-			float y = 0.0f;
-			for (int i = 0; i < 360 / degreesPerSegment + 1; i++)
-			{
-				float currentAngleInRadians = currentAngle * Mathf.PI / 180;
-				float x =  orbit.center.x + orbit.distance * Mathf.Cos(currentAngleInRadians);
-				float z =  orbit.center.z + orbit.distance * Mathf.Sin(currentAngleInRadians);
-
-				lineRenderer.SetPosition(i, new Vector3(x, y, z));
-				currentAngle += degreesPerSegment;
-			}
-			// We don't want things bumping into our orbit lines. They're just for decoration, bro!
-			//lineRenderer.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
 		}
+		//float radius = orbit.distance;
+		//float thetaScale = 1.0f;             //Set lower to add more points
+		//int size = Mathf.RoundToInt(radius * Mathf.PI / thetaScale); //Total number of points in circle.
+
+		//lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		//lineRenderer.SetColors(c1, c2);
+		lineRenderer.SetWidth(0.5F, 0.5F);
+		//lineRenderer.SetVertexCount(360);
+
+		
+		//int i = 0;
+		//for(float theta = 0; theta < radius * Mathf.PI; theta += thetaScale) {
+		//	float x = radius * Mathf.Cos(theta);
+		//	float y = radius * Mathf.Sin(theta);
+		//	
+		//	Vector3 pos = new Vector3(x, y, 0);
+		//	lineRenderer.SetPosition(i, pos);
+		//	i+=1;
+		//}
+
+		float degreesPerSegment = 0.1f;
+		float currentAngle = 0.0f;
+		lineRenderer.SetVertexCount(Mathf.RoundToInt(360 / degreesPerSegment + 1));
+
+		float y = 0.0f;
+		for (int i = 0; i < 360 / degreesPerSegment + 1; i++)
+		{
+			float currentAngleInRadians = currentAngle * Mathf.PI / 180;
+			float x =  orbit.center.x + orbit.distance * Mathf.Cos(currentAngleInRadians);
+			float z =  orbit.center.z + orbit.distance * Mathf.Sin(currentAngleInRadians);
+
+			lineRenderer.SetPosition(i, new Vector3(x, y, z));
+			currentAngle += degreesPerSegment;
+		}
+		// We don't want things bumping into our orbit lines. They're just for decoration, bro!
+		//lineRenderer.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
+
 	}
 }
