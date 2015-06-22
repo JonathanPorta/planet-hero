a569e1d92fdc41164ebde8cfc5edc43a5ba59475
Jonathan Porta <jonathan@jonathanPorta.com>
2015-06-21 22:23:16 -0700

    Created new planet scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public static class RendererExtensions
+{
+	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+}
+
+
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 10.0f;
@@ -23,15 +33,35 @@ public class PlayerController : MonoBehaviour {
 	void Update(){
 		// If we aren't on a planet or don't have a character, bail.
 		if (planet == null || character == null) { return ;}
-		
+
 		playerOrbit.center = CalculateCenterOfGravity();
-		playerOrbit.SetAngleInDegrees(playerOrbit.GetAngleInDegrees() + speed * Input.GetAxis("Horizontal"));
+		float startingAngle = playerOrbit.GetAngleInDegrees();
+		float nextAngle = playerOrbit.GetAngleInDegrees() + speed * -Input.GetAxis("Horizontal");
+		playerOrbit.SetAngleInDegrees(nextAngle);
+
+		if(!OnScreen(playerOrbit.CalculatePosition())){
+			// reset the angle
+			Debug.Log("NEW ANGLE OFF SCREEN");
+			playerOrbit.SetAngleInDegrees(startingAngle);
+		}
+
+		// Set the new position
 		transform.position = playerOrbit.CalculatePosition();
 		transform.LookAt(playerOrbit.center);
-		 
+			 
 		if(Input.GetButton("Jump")){
 			Firing();
 		}
+	}
+
+	private bool OnScreen(Vector3 position){
+		Debug.Log("testing pos:");Debug.Log(position);
+		float buffer = 1.0f;
+		Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, Mathf.Abs(Camera.main.transform.position.y)));
+		Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, Mathf.Abs(Camera.main.transform.position.y)));
+		Debug.Log("bottomLeft: ");Debug.Log(bottomLeft);Debug.Log("topRight: ");Debug.Log(topRight);
+
+		return position.x-buffer > bottomLeft.x && position.x+buffer < topRight.x && position.z+buffer < topRight.z && position.z-buffer > bottomLeft.z;
 	}
 
 	private void Firing(){
@@ -45,7 +75,7 @@ public class PlayerController : MonoBehaviour {
 
 		// Setup orbit based on selected planet
 		float orbitHeight = planet.GetComponent<SphereCollider>().radius * planet.transform.localScale.x + characterPrefab.transform.localScale.x;
-		playerOrbit = new Orbit(CalculateCenterOfGravity(), orbitHeight, 0.0f, 10.0f);
+		playerOrbit = new Orbit(CalculateCenterOfGravity(), orbitHeight, 90.0f, 10.0f);
 		
 		if(character == null){
 			//this.character = Instantiate(characterPrefab, playerOrbit.CalculatePosition(), Quaternion.identity) as GameObject;
